2de32d6ed683da3415003c7e926880c545c0d9dd
gsingh98 <32760012+gsingh98@users.noreply.github.com>
2019-11-09 18:32:54 -0800

    Unzip file working

--- src/unzip_files.py
+++ src/unzip_files.py
@@ -2,22 +2,34 @@ from zipfile import ZipFile
 import os
 import shutil
 import numpy as np
+from tqdm import tqdm
 
+# Assumes that the actual fma_full.zip is in external hard drive. Adjust this
+# according the computer preferences
 zip_location = "F:/fma_full.zip"
 
 
 # Given the name of a file containing directory information for other files
-# this function will unzip the required files in to a folder named unzippped
-# from the zipfile defined in zip_location.
-# This folder will have the same structure as the zip file that the data is
-# being extracted from. If there is already a folder called unzipped it will
-# be deleted before extracting more files.
-def unzip_files(file_name):
-    if file_name is None:
-        raise Exception("Genre is not defined")
-    if os.path.isdir("F:/"):
+# it will unzip the required files from zip_location in to a folder named ../data/unzipped
+# Folder will have the same structure as parent zip file. If ../data/unzipped already
+# exits it will be deleted.
+# Set track_progress to True for progress bar.
+def unzip_files(file_name, track_progress=False):
+    # Clean if directory already exists
+    if os.path.isdir("../data/unzipped"):
         shutil.rmtree("../data/unzipped")
-    files_to_unzip = np.load("data/processed_data/{}".format(file_name))
+    # Decompress required files
+    files_to_unzip = np.load(file_name)
     with ZipFile(zip_location) as zipped:
-        for file in files_to_unzip:
-            zipped.extract(file, "../data/unzipped/")
+        if not track_progress:
+            for file in files_to_unzip:
+                zipped.extract("fma_full/{}".format(file), "../data/unzipped/")
+        else:
+            print("unzipping {}".format(file_name))
+            for file in tqdm(files_to_unzip):
+                zipped.extract("fma_full/{}".format(file), "../data/unzipped/")
+
+
+if __name__ == "__main__":
+    fl = "../data/processed_data/alt_hip_hop/test.npy"
+    unzip_files(fl)
