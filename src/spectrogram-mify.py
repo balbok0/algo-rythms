59a11b7c3baa2c0b53fcad12fbe0eaf5f548fb1c
gsingh98 <32760012+gsingh98@users.noreply.github.com>
2019-11-10 22:17:47 -0800

    Librosa not loading mp3 files

--- src/spectrogram-mify.py
+++ src/spectrogram-mify.py
@@ -1,1 +1,18 @@
-import os
+import librosa as lbs
+import matplotlib as plt
+import numpy as np
+
+
+def spectrogrammify():
+    y, sr = lbs.load("../data/unzipped/fma_full/000/000900.mp3")
+    plt.figure(figsize=(12, 8))
+    D = lbs.amplitude_to_db(np.abs(lbs.stft(y)), ref=np.max)
+    plt.subplot(4, 2, 1)
+    lbs.display.specshow(D, y_axis='linear')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Linear-frequency power spectrogram')
+    plt.show()
+
+
+if __name__ == "__main__":
+    spectrogrammify()
