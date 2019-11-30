a8295e3017ddf737a746e0c46259782d5130d484
Guramrit Singh <gsingh98@general.cs.washington.edu>
2019-11-30 14:07:02 -0800

    Logger saving for linux in the labs

--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import os
 import shutil
+import torch
 
 from datetime import datetime, tzinfo
 from pathlib import Path
@@ -16,7 +17,8 @@ class Logger():
         if not os.path.exists(self.log_folder):
             os.makedirs(self.log_folder)
 
-    def save_end(self, kwargs):
+    def save_end(self, model, kwargs):
+        torch.save(model.state_dict(), self.log_folder / 'model.pth')
         for k, v in kwargs.items():
             np.save(self.log_folder / k, v)
 
